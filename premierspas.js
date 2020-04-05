8939423d57285ac5d4420b1ae137048c88169b8a
Wailweb <wail.sdni@gmail.com>
2020-04-05 16:39:26 +0200

    first commit

---
+++ premierspas.js
@@ -0,0 +1,15 @@
+var favoriteColor = "red";
+
+switch (favoriteColor) {
+  case "blue":
+      console.log("wouah! le bleu c'est trop beau");
+
+
+    break;
+    case "red":
+    console.log("j'aimerais bien une voiture rouge");
+
+  default:
+  console.log("Je ne connais pas t'as couleur");
+
+}
